0901177c4b72265defa9563e02f404c3d1db1b30
ARSamogin <121831671+ARSamogin@users.noreply.github.com>
2025-09-01 14:36:24 -0300

    Update senhas.py

--- senhas.py
+++ senhas.py
@@ -2,11 +2,17 @@ import random
 import string
 
 def gerador():
-    caracteres = string.ascii_letters + string.digits + string.punctuation
-    tamanho = int(input("Digite o tamanho da senha: "))
-    senha = ""
-    for i in range(tamanho):
-        senha += random.choice(caracteres)
-    print(senha)
+    while True:
+        print("-~-~-~-GERADOR DE SENHAS-~-~-~-")
+        print("Gere senhas de 6 a 19 caracteres")
+        caracteres = string.ascii_letters + string.digits + string.punctuation
+        senha = ""
+        tam = int(input("Digite o tamanho da senha: "))
+        if tam <= 5 or tam >=20:
+            print("Digite uma quantidade de 6 a 19 caracteres")
+        else:
+            for i in range(tam):
+                senha += random.choice(caracteres)
+        print(f'Senha Gerada: {senha}')
 
 gerador()
